9ccb76b23fb41e290fee0574965137b57435c27f
Accany1 <129660499+Accany1@users.noreply.github.com>
2024-09-05 11:07:35 +0800

    Now running on the HTML page instead of console

--- javascript.js
+++ javascript.js
@@ -1,8 +1,3 @@
-getHumanChoice = () => {
-    const answer = prompt("Rock, Paper or Scissors")
-    return answer.toLowerCase()
-}
-
 getComputerChoice = () => {
     const answerInt = Math.floor(Math.random()*3)
     if (answerInt === 1) {
@@ -18,54 +13,85 @@ getComputerChoice = () => {
 let computerScore = 0
 let humanScore = 0
 
+//Declaration initiation
+const declareDiv = document.querySelector("#declaration")
+const declare = document.createElement('h3')
+declareDiv.appendChild(declare)
+
 playRound = (computerChoice, humanChoice) => {
     if (computerChoice === humanChoice){
-        console.log("Draw!")
+        declare.textContent = "Draw!"
         return
     } else if (computerChoice === "rock" && humanChoice === "scissors") {
         computerScore += 1
-        console.log("You lose! Rock beats Scissors")
+        declare.textContent = "You lose! Rock beats Scissors"
         return computerScore
     } else if (computerChoice === "rock" && humanChoice === "paper") {
         humanScore += 1
-        console.log("You win! Paper beats Rock")
+        declare.textContent = "You win! Paper beats Rock"
         return humanScore
     } else if (computerChoice === "paper" && humanChoice === "rock") {
         computerScore += 1
-        console.log("You lose! Paper beats Rock")
+        declare.textContent = "You lose! Paper beats Rock"
         return computerScore
     } else if (computerChoice === "paper" && humanChoice === "scissors") {
         humanScore += 1
-        console.log("You win! Scissors beats Paper")
+        declare.textContent = "You win! Scissors beats Paper"
         return humanScore
     } else if (computerChoice === "scissors" && humanChoice === "rock") {
         humanScore += 1
-        console.log("You win! Rock beats Scissors")
+        declare.textContent = "You win! Rock beats Scissors"
         return humanScore
     } else if (computerChoice === "scissors" && humanChoice === "paper") {
         computerScore += 1
-        console.log("You lose! Scissors beats Paper")
+        declare.textContent = "You lose! Scissors beats Paper" 
         return computerScore
     } else {
         return "Wrong input"
     }
 }
 
-playGame = () => {
-    for (let i = 0; i < 5; i ++) {
-        let computerChoice = getComputerChoice()
-        let humanChoice = getHumanChoice()
+checkScore = (computerChoice, humanChoice) => {
+    if (computerScore < 5 && humanScore < 5) {
         playRound(computerChoice, humanChoice)
-        console.log(`Computer: ${computerChoice}`)
-        console.log(`Human: ${humanChoice}`)
-        console.log(`Score: Computer:${computerScore} You:${humanScore}`)
     }
-
-    console.log(`Final score: Computer:${computerScore} You:${humanScore}`)
+    
+    if (computerScore === 5) {
+        declare.textContent = 'THE COMPUTER WON'
+        result.textContent = ''
+        score.textContent = `Final score: Computer:${computerScore} You:${humanScore}`
+    } else if (humanScore === 5) {
+        declare.textContent = 'YOU WON'
+        result.textContent = ''
+        score.textContent = `Final score: Computer:${computerScore} You:${humanScore}`
+    } else {
+        result.textContent = `Computer: ${computerChoice} Human: ${humanChoice}`
+        score.textContent = `Final score: Computer:${computerScore} You:${humanScore}`
+    }
 }
 
+const resultDiv = document.querySelector("#result")
+const result = document.createElement('p')
+resultDiv.appendChild(result)
 
-playGame()
+const scoreDiv = document.querySelector("#score")
+const score = document.createElement('p')
+scoreDiv.appendChild(score)
 
+const rockBtn = document.querySelector('#rock')
+rockBtn.addEventListener("click", () => {
+    let computerChoice = getComputerChoice()
+    checkScore(computerChoice, 'rock')
+})
 
+const paperBtn = document.querySelector('#paper')
+paperBtn.addEventListener("click", () => {
+    let computerChoice = getComputerChoice()
+    checkScore(computerChoice, 'paper')
+})
+const scissorsBtn = document.querySelector('#scissors')
+scissorsBtn.addEventListener("click", () => {
+    let computerChoice = getComputerChoice()
+    checkScore(computerChoice, 'scissors')
+})
 
